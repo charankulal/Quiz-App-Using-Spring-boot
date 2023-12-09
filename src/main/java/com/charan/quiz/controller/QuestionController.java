9d7901cba3c7ee0ce117ab024bc84329114a439e
Charan <charankulal0241@gmail.com>
2023-12-09 21:38:46 +0530

    Fetched questions based on category

--- src/main/java/com/charan/quiz/controller/QuestionController.java
+++ src/main/java/com/charan/quiz/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.charan.quiz.Question;
 import com.charan.quiz.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,11 @@ public class QuestionController {
     public List<Question> getAllQuestions(){
 
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+
+        return questionService.getQuestionsByCategory(category);
     }
 }
