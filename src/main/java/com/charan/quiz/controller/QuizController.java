da760ec40cd16ce1ececa7b26d0646bfb548251c
Charan <charankulal0241@gmail.com>
2023-12-11 22:07:06 +0530

    Completed the getQuizQuestions and submitQuiz method.

--- src/main/java/com/charan/quiz/controller/QuizController.java
+++ src/main/java/com/charan/quiz/controller/QuizController.java
@@ -1,13 +1,14 @@
 package com.charan.quiz.controller;
 
+import com.charan.quiz.model.QuestionWrapper;
+import com.charan.quiz.model.Response;
 import com.charan.quiz.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -20,6 +21,18 @@ public class QuizController {
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizService.createQuiz(category,numQ,title);
 
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id)
+    {
+        return quizService.getQuizQuestions(id);
+
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
     }
 
 }
---
+++ src/main/java/com/charan/quiz/model/Response.java
@@ -0,0 +1,13 @@
+package com.charan.quiz.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+
+    private Integer id;
+    private String response;
+
+}
